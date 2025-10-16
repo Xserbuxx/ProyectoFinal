df96f479937bb977cd849f755fefc9ab7dc7636c
Xserbuxx <sergio.che2107@gmail.com>
2025-10-15 23:24:51 -0500

    cascara del controlador

--- Proyecto/src/co/edu/unbosque/controller/Controlador.java
+++ Proyecto/src/co/edu/unbosque/controller/Controlador.java
@@ -1,19 +1,43 @@
 package co.edu.unbosque.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+
 import co.edu.unbosque.model.ModelFacade;
 import co.edu.unbosque.view.ViewFacade;
 
-public class Controlador {
+public class Controlador implements ActionListener{
 	
 	private ModelFacade mf;
 	private ViewFacade vf;
+	private Properties prop;
 	
 	public Controlador() {
 		mf = new ModelFacade();
 		vf = new ViewFacade();
+		prop = new Properties();
 	}
 	
 	public void run() {
+		agregarOyentes();
+	}
+	
+	private void agregarOyentes() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		switch (e.getActionCommand()) {
+		case "":
+			
+			break;
+
+		default:
+			break;
+		}
 		
 	}
 	
